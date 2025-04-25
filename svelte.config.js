dffcfd3d6a532f85f380acaa8b4e0ffff8cc8672
Alexander Ulbrich <adulbrich@users.noreply.github.com>
2025-04-25 13:07:33 -0700

    removed relative false

--- svelte.config.js
+++ svelte.config.js
@@ -8,8 +8,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
-			relative: false
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
