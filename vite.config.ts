7917c4c1eba4c8a1d3aaad4fd1702669e537cdc6
Alex Ulbrich <adulbrich@users.noreply.github.com>
2025-04-25 10:41:47 -0700

    Initialize cicd

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [tailwindcss(), sveltekit()],
+	resolve: process.env.VITEST
+	? {
+			conditions: ['browser']
+		}
+	: undefined
+});
